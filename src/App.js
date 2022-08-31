1068a5971cd420578c3c70fe465f9f029ddf58c6
Talita Saez <tltsaez@gmail.com>
2022-08-31 17:22:07 -0300

    resolução req 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-//
+import { BrowserRouter, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+      </BrowserRouter>
+
+    );
   }
 }
 
